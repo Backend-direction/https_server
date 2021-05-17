7e44c5bb96511c62a8e6a86781de5266c0a5bf9e
vovapankiw <vovapankiw@gmail.com>
2021-05-17 08:57:34 +0300

    added certificate via open ssl

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/**
+ * Create server as below
+ * Generate key with openssl openssl genrsa -out key.pem
+ * Generate itermidiate crt openssl req -new -key key.pem -out csr.pem
+ * Generate final cert openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out cert.pem
+ */
 const express = require('express');
 const https = require('https');
 const path = require('path');
@@ -9,9 +15,12 @@ app.use('/', (req, res, next) => {
   res.send('Hello from server!');
 });
 
-const sslServer = https.createServer({
-  key: '',
-  cert: ''
-});
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+  },
+  app
+);
 
 sslServer.listen(3443, () => console.log('secure server'));
